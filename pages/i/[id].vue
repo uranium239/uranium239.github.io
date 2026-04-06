4cc111f2222847c517e3171ae14eebaceccb58df
uranium239 <kolchin00@yandex.ru>
2026-04-06 21:54:41 +0300

    Revert "123fix"
    This reverts commit df9db6e53a52ca41a1fe279a128c9d143f97096b.

--- pages/i/[id].vue
+++ pages/i/[id].vue
@@ -18,7 +18,7 @@ const getGenderEnding = () => {
   const gender = guest.value?.gender;
   if (gender === "male") return "ой";
   if (gender === "female") return "ая";
-  if (gender === "family") return "ие";
+  if (gender === 'helicopter') return 'ие';
   return "";
 }
 
